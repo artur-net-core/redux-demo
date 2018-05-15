48f89af7e790d4fd1ac01badc306931346beac52
Artur Kordzik <artur.kordzik@adform.com>
2018-05-15 09:53:19 +0200

    connecting to redux fixes

--- src/courses/coursesPage.js
+++ src/courses/coursesPage.js
@@ -1,4 +1,6 @@
-import React, { PropTypes } from "react";
+import * as courseActions from '../actions/courseActions';
+import React, { PropTypes } from 'react';
+import {connect} from 'react-redux';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -19,13 +21,18 @@ class CoursesPage extends React.Component {
     }
 
     onClickSave() {
-        alert(`saving ${this.state.course.title}`);
+        this.props.dispatch(courseActions.createCourse(this.state.course));
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
     }
     
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input 
                     type="text"
@@ -40,4 +47,15 @@ class CoursesPage extends React.Component {
     }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        courses: state.courses
+    };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
